4173a17c0784ad3bfed2ba07dd5cd697d22259dd
Kshitiz <kshitizbabik.37@gmail.com>
2024-06-16 17:19:57 +0545

    Social Media Button Removed.

--- src/main/java/com/example/demo/utlis/VerifyRecaptcha.java
+++ src/main/java/com/example/demo/utlis/VerifyRecaptcha.java
@@ -15,8 +15,8 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyRecaptcha {
 
 	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-//	public static final String secret = "6LcNy8YpAAAAAAnrsMSWY6c--rPKiKVncfSynr3A";
-	public static final String secret = "6Le5a_QpAAAAAPoI3um_wUQFjdDZW30POqRMxqWl";
+	public static final String secret = "6LcNy8YpAAAAAAnrsMSWY6c--rPKiKVncfSynr3A";
+//	public static final String secret = "6Le5a_QpAAAAAPoI3um_wUQFjdDZW30POqRMxqWl";
 	private final static String USER_AGENT = "Mozilla/5.0";
 	
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
